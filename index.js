f6ca4b5af3f86c20058f04a892cb746b990515df
ScofieldaCreep <tju_scofield@outlook.com>
2024-03-31 06:06:14 -0400

    Update resource data and API endpoints

--- index.js
+++ index.js
@@ -1,58 +1,86 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
-// const cors = require("cors");
 
-// require 是 node.js 的一个全局函数，用于引入模块
-const path = require("path");
-// resolve 是 path 的一个方法，用于将路径或路径片段的序列解析为绝对路径
-const pathToFile = path.resolve("./data.json");
-// fs 是 node.js 的一个全局模块，用于操作文件
 const fs = require("fs");
+const path = require("path");
+const pathToFile = path.resolve("./data.json");
 
-// var corsOptions = {
-//   origin: "http://localhost:3000",
-//   optionsSuccessStatus: 200,
-// };
-
-// app.use(cors(corsOptions));
-
-const getResources = JSON.parse(fs.readFileSync(pathToFile));
+const getResources = () => JSON.parse(fs.readFileSync(pathToFile));
 
 app.use(express.json());
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/api/resources/:id", (req, res) => {
+    const resources = getResources();
+    const { id } = req.params;
+    const resource = resources.find((resource) => resource.id === id);
+    res.send(resource);
+});
+
+app.get("/api/activeresource", (req, res) => {
+    const resources = getResources();
+    const activeResource = resources.find(
+        (resource) => resource.status === "active",
+    );
+    res.send(activeResource);
+});
+
+app.patch("/api/resources/:id", (req, res) => {
+    const resources = getResources();
+    const { id } = req.params;
+    const index = resources.findIndex((resource) => resource.id === id);
+
+    const activeResource = resources.find(
+        (resource) => resource.status === "active",
+    );
+
+    resources[index] = req.body;
+    if (req.body.status === "active") {
+        if (activeResource) {
+            return res
+                .status(422)
+                .send("There is already one active resource!");
+        }
+        resources[index].status = "active";
+        resources[index].activationTime = new Date();
+    }
+
+    fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+        if (error) {
+            return res.status(422).send("Cannot store data in the file!");
+        }
+
+        return res.send("Data has been updated!");
+    });
+});
 
 app.get("/api/resources", (req, res) => {
-  const resources = getResources;
-  res.send(resources);
+    const resources = getResources();
+    res.send(resources);
 });
 
 app.post("/api/resources", (req, res) => {
-  const resources = getResources;
-  const resource = req.body;
-  resource.createdAt = new Date();
-  resource.status = "inactive";
-  resource.id = Date.now().toString();
-  resources.unshift(resource);
-  // unshift() 方法可向数组的开头添加一个或更多元素，并返回新的长度
-  fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
-    if (error) {
-      return res.status(422).send("Data cannot be stored!");
-    }
-    return res.send("Data has been received!");
-  });
+    const resources = getResources();
+    const resource = req.body;
+
+    resource.createdAt = new Date();
+    resource.status = "inactive";
+    resource.id = Date.now().toString();
+    resources.unshift(resource);
+
+    fs.writeFile(pathToFile, JSON.stringify(resources, null, 2), (error) => {
+        if (error) {
+            return res.status(422).send("Cannot store data in the file!");
+        }
+
+        return res.send("Data has been saved!");
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-// :id 是一个动态路由参数, /api/resources/21938hkjkwqgf 会被解析为 { id: "21938hkjkwqgf" }
-app.get("/api/resources/:id", (req, res) => {
-  const resources = getResources;
-  // 也可以写成 const id = req.params.id
-  const { id } = req.params;
-  const resource = resources.find((resource) => resource.id === id);
-  res.send(resource);
+    console.log("Server is listening on port:" + PORT);
 });
